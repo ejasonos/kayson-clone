558432c5eb23a86b3bc7d0bf81d6c02d965da1f0
Favour E. Onosemuode <favejakp@gmaill.com>
2025-10-07 07:56:27 +0100

    updated fronted

--- src/views/OurServices.vue
+++ src/views/OurServices.vue
@@ -4,51 +4,54 @@ const webStore = useWebStore();
 </script>
 <template>
   <!-- id="tops" -->
-  <section class="flex flex-col xl:p-20">
-    <ul
-      class="flex flex-wrap items-center justify-around py-10 gap-y-10 xl:grid xl:grid-cols-3 bg-gray-300"
+  <section class="flex flex-col xl:p-20 bg-gray-200">
+        <ul
+      class="flex flex-wrap items-center justify-around py-10 gap-y-10 xl:grid xl:grid-cols-3"
     >
       <li
         v-for="service in webStore.vehicleServicesA"
         v-bind:key="service.id"
-        class="relative w-[300px] flex flex-col justify-center p-6 m-2 xl:p-10 bg-white"
+        class="flex justify-center items-center"
       >
-        <div
-          class="absolute -top-8 flex justify-center self-center p-4 h-16 w-16 bg-green-600 rounded-sm"
-        >
-          <img v-bind:src="service.image" class="filter invert-95" />
-        </div>
-        <div class="flex flex-col space-y-2 mt-3">
-          <p class="text-md text-center text-gray-600 font-semibold">
-            {{ service.title }}
-          </p>
-          <p class="text-xs font-semibold text-gray-800/50 text-center">
-            {{ service.info }}
-          </p>
+        <div class="relative w-[300px] flex flex-col justify-center p-6 m-2 xl:p-10 bg-white">
+          <div
+            class="absolute -top-8 flex justify-center self-center p-4 h-16 w-16 bg-green-600 rounded-sm"
+          >
+            <img v-bind:src="service.image" class="filter invert-95" />
+          </div>
+          <div class="flex flex-col space-y-2 mt-3">
+            <p class="text-md text-center text-gray-600 font-semibold">
+              {{ service.title }}
+            </p>
+            <p class="text-xs font-semibold text-gray-800/50 text-center">
+              {{ service.info }}
+            </p>
+          </div>
         </div>
       </li>
     </ul>
-
     <ul
-      class="flex flex-wrap items-center justify-around py-10 gap-y-10 xl:grid xl:grid-cols-3 bg-gray-300"
+      class="flex flex-wrap items-center justify-around py-10 gap-y-10 xl:grid xl:grid-cols-3"
     >
       <li
         v-for="service in webStore.vehicleServicesB"
         v-bind:key="service.id"
-        class="relative w-[300px] flex flex-col justify-center items-center p-6 m-2 xl:p-10 bg-white"
+        class="flex justify-center items-center"
       >
-        <div
-          class="absolute -top-8 flex justify-center self-center p-4 h-16 w-16 bg-green-600 rounded-sm"
-        >
-          <img v-bind:src="service.image" class="filter invert-95" />
-        </div>
-        <div class="flex flex-col space-y-2 mt-3">
-          <p class="text-md text-center text-gray-600 font-semibold">
-            {{ service.title }}
-          </p>
-          <p class="text-xs font-semibold text-gray-800/50 text-center">
-            {{ service.info }}
-          </p>
+        <div class="relative w-[300px] flex flex-col justify-center p-6 m-2 xl:p-10 bg-white">
+          <div
+            class="absolute -top-8 flex justify-center self-center p-4 h-16 w-16 bg-green-600 rounded-sm"
+          >
+            <img v-bind:src="service.image" class="filter invert-95" />
+          </div>
+          <div class="flex flex-col space-y-2 mt-3">
+            <p class="text-md text-center text-gray-600 font-semibold">
+              {{ service.title }}
+            </p>
+            <p class="text-xs font-semibold text-gray-800/50 text-center">
+              {{ service.info }}
+            </p>
+          </div>
         </div>
       </li>
     </ul>
